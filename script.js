e18a221784ca2d167352e3af10e468fa4c35ad3e
Jayaveerasabari Velusamy <jayaveerasabarivelusamy@gmail.com>
2023-05-25 18:15:32 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+setTimeout(()=>
+{
+
+    let FN = document.createElement('div')
+    FN.innerHTML = '10';
+    document.body.append(FN);
+   
+    setTimeout(()=>
+    {
+        let heading2 = document.createElement('div');
+        heading2.innerHTML = '9';
+        document.body.append(heading2);
+    },1000)
+
+
+    setTimeout(()=>
+    {
+        let heading = document.createElement('div');
+        heading.innerHTML = '8';
+        document.body.append(heading);
+
+    },1000)
+
+    setTimeout(()=>{
+        let heading = document.createElement('div');
+        heading.innerHTML = '7';
+        document.body.append(heading);
+
+    },1000)
+    setTimeout(()=>{
+        let heading = document.createElement('div');
+    heading.innerHTML = '6';
+    document.body.append(heading);
+
+    },1000)
+    setTimeout(()=>{
+
+        let heading = document.createElement('div');
+        heading.innerHTML = '5';
+        document.body.append(heading);
+    },1000)
+    setTimeout(()=>{
+        let heading = document.createElement('div');
+        heading.innerHTML = '4';
+        document.body.append(heading);
+
+    },1000)
+    setTimeout(()=>{
+
+        let heading = document.createElement('div');
+        heading.innerHTML = '3';
+        document.body.append(heading);
+    },1000)
+    setTimeout(()=>{
+        let heading = document.createElement('div');
+        heading.innerHTML = '2';
+        document.body.append(heading);
+
+    },1000)
+    setTimeout(()=>{
+        let heading = document.createElement('div');
+        heading.innerHTML = '1';
+        document.body.append(heading);
+    },1000)
+    setTimeout(()=>
+    {
+        let heading = document.createElement('div');
+        heading.innerHTML = 'Happy Independence Day';
+        document.body.append(heading);
+    },1000)
+},1000)
+
