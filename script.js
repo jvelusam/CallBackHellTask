2a0b0a54e8ffaf74f9f02abd85acd594d759c21f
Jayaveerasabari Velusamy <jayaveerasabarivelusamy@gmail.com>
2023-05-26 16:14:24 +0530

    updated timer

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 setTimeout(() => {
     let h1 = document.getElementById('one')
     h1.style.visibility = 'visible'
-
     setTimeout(() => {
         let two = document.getElementById('two')
         two.style.visibility = 'visible'
-
         setTimeout(() => {
             let three = document.getElementById('three')
             three.style.visibility = 'visible'
@@ -43,7 +41,6 @@ setTimeout(() => {
             }, 2000)
 
         }, 2000)
-
 
     }, 2000)
 
