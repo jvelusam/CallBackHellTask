620833034ca13b5909adb941dc48aa40ee4de2a1
Jayaveerasabari Velusamy <jayaveerasabarivelusamy@gmail.com>
2023-05-26 11:51:04 +0530

    updated the value

--- script.js
+++ script.js
@@ -1,72 +1,62 @@
 setTimeout(()=>
 {
 
-    let FN = document.createElement('div')
-    FN.innerHTML = '10';
-    document.body.append(FN);
+    let h1 = document.getElementbyId('one')
+    h1.style.visibility='visible'
+
    
     setTimeout(()=>
     {
-        let heading2 = document.createElement('div');
-        heading2.innerHTML = '9';
-        document.body.append(heading2);
+        let h2 = document.getElementbyId('two')
+        h2.style.visibility='visible'
     },1000)
 
 
     setTimeout(()=>
     {
-        let heading = document.createElement('div');
-        heading.innerHTML = '8';
-        document.body.append(heading);
+        let h3 = document.getElementbyId('three')
+        h3.style.visibility='visible'
 
     },1000)
 
     setTimeout(()=>{
-        let heading = document.createElement('div');
-        heading.innerHTML = '7';
-        document.body.append(heading);
+        let h4 = document.getElementbyId('four')
+        h4.style.visibility='visible'
 
     },1000)
     setTimeout(()=>{
-        let heading = document.createElement('div');
-    heading.innerHTML = '6';
-    document.body.append(heading);
-
-    },1000)
-    setTimeout(()=>{
-
-        let heading = document.createElement('div');
-        heading.innerHTML = '5';
-        document.body.append(heading);
-    },1000)
-    setTimeout(()=>{
-        let heading = document.createElement('div');
-        heading.innerHTML = '4';
-        document.body.append(heading);
+        let h5 = document.getElementbyId('five')
+        h5.style.visibility='visible'
 
     },1000)
     setTimeout(()=>{
 
-        let heading = document.createElement('div');
-        heading.innerHTML = '3';
-        document.body.append(heading);
+        let h6 = document.getElementbyId('six')
+        h6.style.visibility='visible'
     },1000)
     setTimeout(()=>{
-        let heading = document.createElement('div');
-        heading.innerHTML = '2';
-        document.body.append(heading);
+        let h7 = document.getElementbyId('seven')
+        h7.style.visibility='visible'
 
     },1000)
     setTimeout(()=>{
-        let heading = document.createElement('div');
-        heading.innerHTML = '1';
-        document.body.append(heading);
+
+        let h8 = document.getElementbyId('eight')
+        h8.style.visibility='visible'
+    },1000)
+    setTimeout(()=>{
+        let h9 = document.getElementbyId('nine')
+        h9.style.visibility='visible'
+
+    },1000)
+    setTimeout(()=>{
+        let h10 = document.getElementbyId('ten')
+        h10.style.visibility='visible'
     },1000)
     setTimeout(()=>
     {
-        let heading = document.createElement('div');
-        heading.innerHTML = 'Happy Independence Day';
-        document.body.append(heading);
+        let h11 = document.getElementbyId('text')
+        h11.style.visibility='visible'
     },1000)
 },1000)
 
