afec71909758e3a2dfece024a8b54c1504378b55
Jayaveerasabari Velusamy <jayaveerasabarivelusamy@gmail.com>
2023-05-26 12:01:42 +0530

    Timer updates

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ setTimeout(()=>
     {
         let h2 = document.getElementbyId('two')
         h2.style.visibility='visible'
-    },1000)
+    },3000)
 
 
     setTimeout(()=>
@@ -17,46 +17,46 @@ setTimeout(()=>
         let h3 = document.getElementbyId('three')
         h3.style.visibility='visible'
 
-    },1000)
+    },3000)
 
     setTimeout(()=>{
         let h4 = document.getElementbyId('four')
         h4.style.visibility='visible'
 
-    },1000)
+    },3000)
     setTimeout(()=>{
         let h5 = document.getElementbyId('five')
         h5.style.visibility='visible'
 
-    },1000)
+    },3000)
     setTimeout(()=>{
 
         let h6 = document.getElementbyId('six')
         h6.style.visibility='visible'
-    },1000)
+    },3000)
     setTimeout(()=>{
         let h7 = document.getElementbyId('seven')
         h7.style.visibility='visible'
 
-    },1000)
+    },3000)
     setTimeout(()=>{
 
         let h8 = document.getElementbyId('eight')
         h8.style.visibility='visible'
-    },1000)
+    },3000)
     setTimeout(()=>{
         let h9 = document.getElementbyId('nine')
         h9.style.visibility='visible'
 
-    },1000)
+    },3000)
     setTimeout(()=>{
         let h10 = document.getElementbyId('ten')
         h10.style.visibility='visible'
-    },1000)
+    },3000)
     setTimeout(()=>
     {
         let h11 = document.getElementbyId('text')
         h11.style.visibility='visible'
-    },1000)
-},1000)
+    },3000)
+},3000)
 
